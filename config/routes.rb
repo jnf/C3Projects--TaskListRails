a818ea41db3b40d757e6d9a1d466996de4c54931
jnf <jeremy.flores@gmail.com>
2015-06-18 08:46:35 -0700

    Fix my seeds and start working on wave 2 reqs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'task#index'
-  get '/task/:id' => 'task#show'
 
+  get  '/task/:id' => 'task#show'
+  get  '/task/new' => 'task#new'
+  post '/task/create' => 'task#create'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,16 @@ def random_time
 end
 
 tasks = [
-  { name: "The First Task", description: "", completed_at: random_time },
-  { name: "Go to Brunch", description: "" },
-  { name: "Go to Lunch", description: "", completed_at: random_time },
-  { name: "Go to Second Lunch", description: "" },
-  { name: "Play Video Games", description: "", completed_at: random_time },
-  { name: "High Five Somebody You Don't Know", description: "", completed_at: random_time },
-  { name: "Plant Flowers", description: "", completed_at: random_time },
-  { name: "Call Mom", description: "" },
-  { name: "She worries, you know.", description: "" },
-  { name: "Nap.", description: "", completed_at: random_time }
+  { name: "The First Task", description: "You should do this one first.", completed_at: random_time },
+  { name: "Go to Brunch", description: "Vittles is pretty good. Or Lola, if you got paid this week." },
+  { name: "Go to Lunch", description: "Rocco's or maybe just stay home and have a sandwich. :)", completed_at: random_time },
+  { name: "Go to Second Lunch", description: "Second Lunch is code for coffee and pastry. Yay pastries!" },
+  { name: "Play Video Games", description: "Dying Light is soooo scary, but it's a lot of fun.", completed_at: random_time },
+  { name: "High Five Somebody You Don't Know", description: "Stranger danger? Hardly. Express friendliness to those around you.", completed_at: random_time },
+  { name: "Plant Flowers", description: "Make the world a more beautiful place. Except for folks with allergies. Sorry allergy peeps. :(", completed_at: random_time },
+  { name: "Call Mom", description: "Seriously, it's been awhile." },
+  { name: "She worries, you know.", description: "I know, I know. I'll call her. Promise." },
+  { name: "Nap.", description: "YUS", completed_at: random_time }
 ]
 
 tasks.each do |task|
